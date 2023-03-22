0f685962a617ec04959dbbfbb6500d8af21ebf5f
JS <51195305+sombra2@users.noreply.github.com>
2023-03-22 11:40:07 +0100

    adding the module 'listener.py' to enable the script to listen for retweets and new followers

---
+++ listener.py
@@ -0,0 +1,34 @@
+# this is the listening script for users retweeting and new follows
+
+import tweepy
+
+consumer_key = credentials.consumer_key
+consumer_secret = credentials.consumer_secret
+access_token = credentials.access_token
+access_token_secret = credentials.access_token_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create stream listener
+class MyStreamListener(tweepy.StreamListener):
+    def on_follow(self, follower):
+        # Send tweet to new follower
+        api.update_status(f"Gracias por seguirnos, @{follower.screen_name}!")
+
+# Create stream object and start listening
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+myStream.filter(track=["@your_bot_name"])
+
+# Get recent tweets and their retweets
+tweets = api.user_timeline(screen_name="your_bot_name", count=20, include_rts=True)
+
+# Like retweets
+for tweet in tweets:
+    retweets = api.retweets(tweet.id)
+    for retweet in retweets:
+        api.create_favorite(retweet.id)
